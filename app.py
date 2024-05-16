7db0994a38b0d39f62bb2e3a46ebec61e3b6e399
MattNandavong <146684932+MattNandavong@users.noreply.github.com>
2024-05-17 01:27:40 +1000

    Update app.py
    remove manual load the model. insert QA model API

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify, send_from_directory
 import os
-from transformers import RobertaTokenizer, RobertaForQuestionAnswering
+from transformers import RobertaTokenizer, RobertaForQuestionAnswering, pipeline
 import torch
 from flask_cors import CORS
 
@@ -19,29 +19,16 @@ def index():
 @app.route('/query', methods=['POST'])
 def process_query():
     global message_history  # Use global variable to store message history
+    # load QA model API
+    get_answer = pipeline(task="question-answering", model="MattNandavong/QA-roberta-model3")
 
-    # Load pre-trained BERT model and tokenizer
-    model_name = "MattNandavong/QA_model8-test"
-    tokenizer = RobertaTokenizer.from_pretrained(model_name)
-    model = RobertaForQuestionAnswering.from_pretrained("MattNandavong/QA-roberta-model")
-    context = "While a bachelor's degree is a common requirement for MBA programs, some institutions offer pathways for individuals without a bachelor's degree based on relevant work experience or related graduate certificates. We can potentially offer students unit exemptions for our undergraduate courses, based on diploma and higher education qualifications undertaken within the last 10 years. Previous studies don’t necessarily need to be related to the field you’re looking to pursue with us to make you eligible for exemptions. We can also award unit exemptions towards our postgraduate courses, where previously completed studies are considered academically equivalent (i.e. postgraduate level) and have been completed within the last 10 years. TAFE students may be eligible to receive credit for previous study and work experience. If you’ve completed units or certificates at another recognised institute or training organisation, you may be able to apply for a credit transfer. Alternatively, if you have significant work experience related to your studies, you can apply to have this recognised through recognition of prior learning. Swinburne Online takes domestic students for both degrees and TAFE diplomas from anywhere in Australia. International students are also accepted for our degrees, although some differences to their payment processes will apply.  If you are applying to study a degree at Swinburne Online, enrolments close approximately 10 days before the start of each teaching period. Our teaching periods run three times a year, from March to June, July to October and November to February. For TAFE students, you can enrol at any time and begin your self-paced studies at a variety of start dates that suit you. For full details about start dates, visit our Key date page.  It is expected that applicants have proficient English skills. We recommend the below levels in an English language test such as IELTS or TOEFL to ensure you are successful at university study. It is expected that applicants have proficient English skills. You should have a bachelor degree or equivalent (at least three years) studied in English, or have completed year 12 English in Australia. You must meet the below levels in an English language test such as IELTS, ISLPR or PEAT to ensure you are successful. "
+    # load context
+    context = "We can potentially offer students unit exemptions for our undergraduate courses, based on diploma and higher education qualifications undertaken within the last 10 years. Swinburne Online takes domestic students for both degrees and TAFE diplomas from anywhere in Australia. International students are also accepted for our degrees. enrolments close approximately 10 days before the start of each teaching period. Our teaching periods run three times a year, from March to June, July to October and November to February. It is expected that applicants have proficient English skills. We recommend the below levels in an English language test such as IELTS or TOEFL to ensure you are successful at university It is expected that applicants have proficient English skills. You should have a bachelor degree or equivalent (at least three years) studied in English, or have completed year 12 English in Australia. You must meet the below levels in an English language test such as IELTS, ISLPR or PEAT. recent Year 12 students must apply through VTAC for Swinburne courses for the March intake. However you may apply for Swinburne Online's July or November intakes. If you are unsure which qualification is most relevant to you, speak to a Course Consultant for some advice or see our Levels of Education FAQ section. If you’re unsure about whether you’ll be eligible to study with Swinburne, it’s definitely worth calling our team of course consultants on 1300 069 765 or +61 3 9956 0777 if you are calling from overseas. Our team of Course Consultants will normally get in touch with you within two working days of you submitting your application with an outcome. Students wishing to study with us, simply need to complete an application form and submit it to Swinburne Online. Because you will be studying a degree online, all of our support systems are available over the phone or online. Yes, Swinburne Online accept applications from international students for our higher education courses. You can study at Swinburne Online from any state or territory in Australia"
 
     data = request.get_json()
     query = data['query']
-
-    # Tokenize question and context with truncation
-    inputs = tokenizer(query, context, truncation=True, return_tensors="pt")
-
-    # Perform question answering inference
-    outputs = model(**inputs)
-    start_logits = outputs.start_logits
-    end_logits = outputs.end_logits
-
-    # Get the most likely answer
-    start_index = torch.argmax(start_logits)
-    end_index = torch.argmax(end_logits)
-    answer = tokenizer.convert_tokens_to_string(
-        tokenizer.convert_ids_to_tokens(inputs["input_ids"][0][start_index:end_index + 1]))
+    # get the most likely answer
+    answer = get_answer(context=context, question=query)['answer']
 
     # Add user query and model answer to message history
     message_history.append({'user_query': query, 'model_answer': answer})
