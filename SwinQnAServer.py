f4e075dc976f79f3c08e1b082b47d5e3d80d1766
chamodSamajith <chamodsr@gmail.com>
2024-05-18 14:48:34 +1000

    add data file and importing to flask server

--- SwinQnAServer.py
+++ SwinQnAServer.py
@@ -3,3 +3,8 @@ from sentence_transformers import SentenceTransformer, util
 import pandas as pd
 
 app = Flask(__name__)
+
+# Import Swinburn Frequently asked quetions from a csv file
+df = pd.read_csv('SwinFAQDataSet.csv')
+questions = df['Question'].tolist()
+answers = df['Answer'].tolist()
